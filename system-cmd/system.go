463df845b853c686b57224519b59135e88b2a301
khpark0203 <khpark0203@gmail.com>
2020-12-01 21:06:30 +0900

    Add description of functions.

--- system-cmd/system.go
+++ system-cmd/system.go
@@ -6,30 +6,33 @@ import (
 )
 
 const (
-	EXITSTR = ";echo $?"
-	SYNCSTR = "sync;sync"
+    exit = ";echo $?"
+    sync = "sync;sync"
 )
 
+// Run execute shell command and return "echo $?" int value.
 func Run(command string) int {
-	var ret int
-	if len(command) > 0 {
-		cmd := exec.Command("sh", "-c", command + EXITSTR)
-		stdout, _ := cmd.CombinedOutput()
-		ret, _ = strconv.Atoi(string(stdout))
-	} else {
-		ret = -1
-	}
-	return ret
+    var ret int
+    if len(command) > 0 {
+        cmd := exec.Command("sh", "-c", command + exit)
+        stdout, _ := cmd.CombinedOutput()
+        ret, _ = strconv.Atoi(string(stdout))
+    } else {
+        ret = -1
+    }
+    return ret
 }
 
+// RunRead execute shell command and return output string.
 func RunRead(command string) string {
-	cmd := exec.Command("sh", "-c", command)
-	stdout, _ := cmd.CombinedOutput()
-	ret := string(stdout)
-	return ret
+    cmd := exec.Command("sh", "-c", command)
+    stdout, _ := cmd.CombinedOutput()
+    ret := string(stdout)
+    return ret
 }
 
+// Sync execute sync command in shell.
 func Sync() {
-	cmd := exec.Command("sh", "-c", SYNCSTR)
-	cmd.Run()
+    cmd := exec.Command("sh", "-c", sync)
+    cmd.Run()
 }
